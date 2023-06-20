41ae2a4fc8e32f1f30eac2013a0e2f90c55c23e5
TheWisePigeon <zozozozeph@gmail.com>
2023-06-20 23:28:20 +0000

    setup project

--- cmd/init.go
+++ cmd/init.go
@@ -1,12 +1,15 @@
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/spf13/cobra"
 )
 
 var init_cmd = &cobra.Command{
     Use: "init",
     Run: func(cmd *cobra.Command, args []string) {
+        fmt.Print("Hello")
 
     },
 }
