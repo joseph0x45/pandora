441b3dff5fc4fdf44294471947066b49f3af87fa
TheWisePigeon <zozozozeph@gmail.com>
2023-10-10 23:31:15 +0000

    minor updates

--- cmd/init.go
+++ cmd/init.go
@@ -4,10 +4,16 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var (
+  init_message =
+`Welcome to Pandora, a simple and easy to use password manager.
+You are creating a new Pandora box. Enter the key to the box:`
+)
+
 var init_cmd = &cobra.Command{
 	Use: "init",
 	Run: func(cmd *cobra.Command, args []string) {
-
+    println(init_message)
 
 	},
 }
